7cede929cb24f97ef992d62318e6230f6a98913a
luis perico <luispericodambroski@gmail.com>
2025-03-19 17:05:57 -0300

    <update>[estrutura]: estrura do projeto alterada

--- projetoint/projetoint/wsgi.py
+++ projetoint/projetoint/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for projetoint project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import projetoint
+# projetoint/wsgi.py
+import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoint.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projetoint.settings")
 application = get_wsgi_application()
+
+app = projetoint  # ⚠️ Nome obrigatório para o Vercel detectar
