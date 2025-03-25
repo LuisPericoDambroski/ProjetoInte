6c6f2d330b992ad2e38228d8aa210d12418997f0
luis <luispericodambroski@gmail.com>
2025-03-25 16:25:27 -0300

    asd

--- projetoint/projetoArton/apps.py
+++
@@ -1,6 +0,0 @@
-# projetoArton/apps.py
-from django.apps import AppConfig
-
-class ProjetoArtonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'projetoArton'
--- projetoint/projetoArton/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Acessorio(models.Model):
-    nome = models.CharField(max_length=100)
-    tipo = models.CharField(max_length=10)
-    preco = models.IntegerField()
-    descricao = models.TextField()
