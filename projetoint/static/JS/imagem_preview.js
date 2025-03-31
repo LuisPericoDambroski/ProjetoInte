776fe5a09dd6889726a6d55f31ef1687d896cbb4
Gabriel <gabinepo2201@gmail.com>
2025-03-30 21:19:56 -0300

    Troca de imagens dentro da ficha funcionando

--- projetoint/static/JS/imagem_preview.js
+++ projetoint/static/JS/imagem_preview.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("Preview pronto!");
+  
     const input = document.getElementById("image-input");
     const preview = document.getElementById("preview-image");
-
+  
     if (input && preview) {
-        input.addEventListener("change", function () {
-            const file = this.files[0];
-            if (file) {
-                const reader = new FileReader();
-
-                reader.onload = function (e) {
-                    preview.src = e.target.result;
-                };
-
-                reader.readAsDataURL(file);
-            }
-        });
+      input.addEventListener("change", function () {
+        const file = input.files[0];
+        if (file) {
+          console.log("Arquivo selecionado:", file.name);
+          const reader = new FileReader();
+          reader.onload = function (e) {
+            preview.src = e.target.result;
+          };
+          reader.readAsDataURL(file);
+        }
+      });
     }
-});
+  });
+  
